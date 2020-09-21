771768f83d9c79b8f639e4208c8586108470a1bf
jaehooon <qlenfr0922@gmail.com>
2020-09-21 13:01:51 +0900

    200921 13:01

---
+++ src/ch02/SuccessOrFail.java
@@ -0,0 +1,19 @@
+package ch02;
+import java.util.Scanner;
+
+/**
+ * 예제 2-10 | if 문 활용
+ */
+public class SuccessOrFail {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("점수를 입력하시오 : ");
+        int score = scan.nextInt();
+        if (score >= 80) {
+            System.out.println("축하합니다! 합격입니다.");
+        }
+
+        scan.close();
+    }
+}
